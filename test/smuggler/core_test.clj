0de9896b90f618715454e9f36414d1167bebdc06
Dax Fohl <dax.fohl@gmail.com>
2013-05-23 05:39:26 +0800

    unit test

--- test/smuggler/core_test.clj
+++ test/smuggler/core_test.clj
@@ -1,7 +1,34 @@
 (ns smuggler.core-test
   (:use clojure.test
-        smuggler.core))
+        smuggler.core)
+  (:import smuggler.core.Doll))
+
+(def dolls
+  [(Doll. "luke" 9 150)
+   (Doll. "anthony" 13 35)
+   (Doll. "candice" 153 200)
+   (Doll. "dorothy" 50 160)
+   (Doll. "puppy" 15 60)
+   (Doll. "thomas" 68 45)
+   (Doll. "randal" 27 60)
+   (Doll. "april" 39 40)
+   (Doll. "nancy" 23 30)
+   (Doll. "bonnie" 52 10)
+   (Doll. "marc" 11 70)
+   (Doll. "kate" 32 30)
+   (Doll. "tbone" 24 15)
+   (Doll. "tranny" 48 10)
+   (Doll. "uma" 73 40)
+   (Doll. "grumpkin" 42 70)
+   (Doll. "dusty" 43 75)
+   (Doll. "grumpy" 22 80)
+   (Doll. "eddie" 7 20)
+   (Doll. "tory" 18 12)
+   (Doll. "sally" 4 50)
+   (Doll. "babe" 30 10)])
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "Sample Input"
+    (let [output (m dolls 400)]
+      (is (= (get-value output) 1030))
+      (is (= (count output) 12)))))
