9ce502a5b81e64539c3ccd05364ff890dc1667b9
Dax Fohl <dax.fohl@gmail.com>
2013-05-23 15:45:50 +0800

    docstrings

--- src/smuggler/core.clj
+++ src/smuggler/core.clj
@@ -3,7 +3,9 @@
 
 (defrecord Doll [name weight value])
 
-(defn get-value [seq]
+(defn aggregate-value
+  "Aggregates the value of all the value fields in a seq of Dolls"
+  [seq]
   (reduce #(+ %1 (:value %2)) 0 seq))
 
 (defn m
@@ -22,11 +24,9 @@
         (if (> head-weight w)
           without-head
           (let [with-head (cons head (m tail (- w head-weight)))]
-            (if (> (get-value without-head) (get-value with-head))
+            (if (> (aggregate-value without-head) (aggregate-value with-head))
               without-head
               with-head)))))))
-
-
 
 (def ^:const OK 0)
 (def ^:const NO-FILENAME-GIVEN 1)
@@ -34,28 +34,41 @@
 (def ^:const FILE-EMPTY 3)
 (def ^:const FILE-INVALID 4)
 
-(defn print-dolls [dolls]
+(defn print-dolls
+  "Pretty-prints each doll to the console"
+  [dolls]
   (doseq [doll dolls]
     (prn doll)))
 
-(defn run-with-data [dolls weight]
+(defn run-with-data
+  "Runs the optimization algorithm and prints the results"
+  [dolls weight]
   (let [dolls-to-keep (m dolls weight)]
     (print-dolls dolls-to-keep)))
 
-(defn try-parse-float [s]
+(defn try-parse-float
+  "Parses a float string, or returns nil if string is invalid"
+  [s]
   (try (Float/parseFloat s)
        (catch Exception e
          (do (prn (str "Value string given by \"" s "\" is not valid."))
              nil))))
 
-(defn doll-valid? [doll]
+(defn doll-valid?
+  "Validity check for any null or negative values"
+  [doll]
   (let [name (:name doll)
         weight (:weight doll)
         value (:value doll)]
     (and (not-any? nil? [name weight value])
          (not-any? neg? [weight value]))))
 
-(defn parse-doll [s]
+(defn parse-doll
+  "Parses a doll string, given in format \"name,weight,value\", e.g. \"alice,4,9\".
+  Non-negative floats are accepted for weight and value.
+  Returns nil if string or resulting doll is invalid.
+  Anything after a third comma is silently ignored."
+  [s]
   (let [split (clojure.string/split s #",")
         [name weight-string value-string] split
         [weight value] (map try-parse-float [weight-string value-string])
@@ -65,11 +78,17 @@
       (do (prn (str "Doll string given by \"" s "\" is not valid."))
           nil))))
 
-(defn parse-dolls [strings]
+(defn parse-dolls
+  "Parses the doll-strings into a doll seq, but returns null if any Doll is invalid"
+  [strings]
   (let [dolls (map parse-doll strings)]
     (if (some nil? dolls) nil dolls)))
 
-(defn parse-data [contents]
+(defn parse-data
+  "Parses the full data file into a format `[RETURN-CODE [dolls weight]]`.
+  `contents` is a multi-line string where the first line that contains the `weight` in `float` format,
+  followed by N lines of doll strings (see `parse-doll`)"
+  [contents]
   (let [lines (clojure.string/split-lines contents)]
     (if (or (= lines '()) (= contents ""))
       (do (prn "Requested file is empty")
@@ -83,7 +102,10 @@
           [FILE-INVALID nil]
           [OK result])))))
 
-(defn run-with-contents [contents]
+(defn run-with-contents
+  "Runs the smuggler algorithm given the contents of the a data file.
+  See `parse-data` for the expected format of `contents`."
+  [contents]
   (let [result (parse-data contents)
         [status data] result]
     (if (not= OK status)
@@ -92,18 +114,24 @@
         (do (run-with-data dolls weight)
             OK)))))
 
-(defn contents-or-null [filename]
+(defn contents-or-nil
+  "Returns the contents of a file, or nil if there is an error reading the file"
+  [filename]
   (try (slurp filename)
        (catch Exception e nil)))
 
-(defn run-with-file [filename]
-  (let [contents (contents-or-null filename)]
+(defn run-with-file
+  "Runs the smuggler algorithm with the corresponding input file"
+  [filename]
+  (let [contents (contents-or-nil filename)]
         (if (nil? contents)
           (do (prn (str "File \"" filename "\" not found or cannot be opened"))
               FILE-NOT-FOUND)
           (run-with-contents contents))))
 
-(defn run [args]
+(defn run
+  "Runs the smuggler algorithm given the command-line arguments and returns the exit code"
+  [args]
   (if (= args '())
     (do (prn "Supply a filename at the command prompt")
         NO-FILENAME-GIVEN)
